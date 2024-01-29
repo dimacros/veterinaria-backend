362463c9d5c0d5894b57e3cbaea0680ebdf8496a
Marcos David Huaranga Ortega <dimacros@hotmail.com>
2024-01-28 21:03:30 -0500

    add spring project

---
+++ src/main/java/pe/edu/upc/IS277/Is277Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.IS277;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is277Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is277Application.class, args);
+	}
+
+}
